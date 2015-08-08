8b991ac08a6810fcf5c57b3be0d06a8c5b023ebc
Andrew Straw <strawman@astraw.com>
2015-08-08 23:46:32 +0200

    show something to the javascript console to test if JS is working

--- components/About.jsx
+++ components/About.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 export default class About extends React.Component {
   render () {
+    console.log("hi from about component");
     return (
       <main>
         About component
